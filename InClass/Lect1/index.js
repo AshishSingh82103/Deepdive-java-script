ec9999505f7ae99711b565dbb87f40909294caab
Ashish Singh <singhashish82103@gmail.com>
2024-08-27 14:22:11 +0530

    Initial commit

---
+++ InClass/Lect1/index.js
@@ -0,0 +1,11 @@
+let myFunction = function(item1, item2) {
+    let items = 5;
+    if(items === 5) {
+        console.log(items);
+    } else {
+        console.log('not equal to 5')
+    }
+    return items + 1;
+
+}
+console.log(myFunction(3, 2));
