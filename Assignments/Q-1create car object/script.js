0910ea17af0fb4bb16a2f8a42ed3f440e1d76e2b
Ashish Singh <singhashish82103@gmail.com>
2024-08-29 09:20:32 +0530

    Initial commit

---
+++ Assignments/Q-1create car object/script.js
@@ -0,0 +1,22 @@
+//Q: 1 You need to create an Object in Javascript with the name Car. This object will have three properties with name color, seats, nitros The table given below contains the properties of the object, their data type, and the value for that property.
+
+ 
+// Property of Object	Data Type of Property	value for property
+// color	string	 "white"
+//  seats	number	 2
+//  nitros	boolean	 false
+
+// Create another variable msg which contains a string in it with the value
+// "My car have seats seats and it is of color color"
+// but the highlighted ones should state the specified number of seats and is of a particular color, as defined by the properties of the Car object."
+
+
+// Note: Generate Expected Output section will not work for this question You don't need to console.log anything here, just create the Car object and the msg variable.
+
+// Your code here
+let Car = {
+    color:"white", seats:2, nitros:false,
+}
+// let msg = `My car have ${Car.seats} seats and it is of color ${Car.color}`;
+let msg = "My car have " + Car.seats + " seats and it is of color " + Car.color;
+// console.log(msg);
---
+++ Assignments/Q-2Find the value/script.js
@@ -0,0 +1,8 @@
+// Q:-2 Create an array named arr which contains the first 5 multiples of 2. After this create a variable named val which stores the middle element of the array.
+
+// NOTE:- You don't have to return anything. You just have to initialize the variables and assign them values
+
+// Your code here
+let arr  = [ 2,4,6,8,10]
+let val = arr[2];
+console.log(val);
