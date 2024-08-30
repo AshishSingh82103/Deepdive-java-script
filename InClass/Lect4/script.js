dfb7ea844a717f0fe4a9b9d21528f905d55456ad
Ashish Singh <singhashish82103@gmail.com>
2024-08-30 13:16:31 +0530

    Initisl commit

--- InClass/Lect4/script.js
+++ InClass/Lect4/script.js
@@ -8,9 +8,7 @@ const c = 4;
 c = 5; // this is illegal
 
 // ------------- ---------- execution context -------- ---------------
-//  var => var is personel from the user of the server loading site which i srelatable to the source
-//  let => let is not to personel from the user it will fetch the element from the server site.
-//  const = > const is basically not to update and not to reasigne the value the from the sever site.
-
-
+//  var =>  
+//  let => 
+//  const = > 
 
