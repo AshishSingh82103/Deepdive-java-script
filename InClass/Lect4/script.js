17492a99817b9112d75bf476f8384b1d419bb48b
Ashish Singh <singhashish82103@gmail.com>
2024-08-30 12:02:26 +0530

    Initial commit

--- InClass/Lect4/script.js
+++ InClass/Lect4/script.js
@@ -1,1 +1,16 @@
-// waves 
+var a = 1;
+a = 2;
+
+let b = 2;
+b = 3;
+
+const c = 4;
+c = 5; // this is illegal
+
+// ------------- ---------- execution context -------- ---------------
+//  var => var is personel from the user of the server loading site which i srelatable to the source
+//  let => let is not to personel from the user it will fetch the element from the server site.
+//  const = > const is basically not to update and not to reasigne the value the from the sever site.
+
+
+
