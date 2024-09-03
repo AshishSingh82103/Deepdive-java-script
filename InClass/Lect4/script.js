35afb56106f3d96cc25d3008184b03e6b030ebc1
Ashish Singh <singhashish82103@gmail.com>
2024-09-03 10:09:22 +0530

    Initial commit

--- InClass/Lect4/script.js
+++ InClass/Lect4/script.js
@@ -1,16 +1,18 @@
-var a = 1;
-a = 2;
+// var a = 1;
+// a = 2;
 
-let b = 2;
-b = 3;
+// let b = 2;
+// b = 3;
 
-const c = 4;
-c = 5; // this is illegal
+// const c = 4;
+// c = 5; // this is illegal
 
 // ------------- ---------- execution context -------- ---------------
 //  var => 
 //  let => 
 //  const => 
+
+console.log("abcd"); // abcd
 
 
 
