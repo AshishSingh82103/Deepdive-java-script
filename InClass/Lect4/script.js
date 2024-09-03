57cba04b38711664a82b05c3629a9ef10ecfee6b
Ashish Singh <singhashish82103@gmail.com>
2024-09-03 13:40:00 +0530

    Initial commit

--- InClass/Lect4/script.js
+++ InClass/Lect4/script.js
@@ -12,6 +12,10 @@
 //  let => 
 //  const => 
 
+
+// ---------- -- Scope -- ---------------
+
+// Rule No.1 => The file will be executed in the same time
 console.log("abcd"); // abcd
 
 var a = 12;
@@ -24,6 +28,18 @@ if(true) {
     console.log(c); // 10
 }
 console.log(c);
+
+
+let d = 13;
+console.log(d);
+
+if(true) {
+    console.log(d);
+    let e = 23;
+    console.log(e);
+}
+console.log(e); // error e is not defined
+
 
 
 
