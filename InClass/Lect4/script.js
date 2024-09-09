15017f69e4050c7ca2f66e060fcb5aed7bcf2732
Ashish Singh <singhashish82103@gmail.com>
2024-09-09 09:19:22 +0530

    Initial commit

--- InClass/Lect4/script.js
+++ InClass/Lect4/script.js
@@ -13,40 +13,54 @@
 //  const => 
 
 
-// ---------- -- Scope -- ---------------
+// ---------- ------------ --- Scope --- -------------  ---------------
 
 // Rule No.1 => The java-script file its self global.
-console.log("abcd"); // abcd
+// Rule No.2 => Scope of var => Global and functional
+// Rule No. 3 => Var dosen't care about {}
+
 
 // ------------scope- var -variable-----------------
-var a = 12;
+
+// var a = 12;  // global 
+// console.log(a); // 12
+
+// if(true) {
+//     var c = 10;
+//     console.log(a); // 12 if the parent scope have the variable the the child scope will also have the access
+//     console.log(c); // 10
+// }
+// console.log(c); 
+
+//  -------------   -----   ----     let and const ------  ----    -----------------
+
+//  ======>1. let and const have a scope of {}
+// ======>2. let and const have block scope like we have it with (int, char, String)
+
+// let d = 13; // global
+// console.log(d); // 13
+
+
+// if(true) {
+//     console.log(d); // 13
+//     let e = 23;
+//     console.log(e); // 23
+// }
+// // console.log(e); // error
+
+const a = 12; // global
 console.log(a); // 12
 
-
 if(true) {
-    var c = 10;
-    console.log(a); // 12 if the parent scope have the variable the the child scope will also have the access
-    console.log(c); // 10
+    console.log(a); // 12
+    const b = 13;
+    console.log(b); // 13
 }
-console.log(c); //  
+console.log(b); // error
 
 
-let d = 13;
-console.log(d); // 13
 
-// -------------Scoping let-variable------------------
-if(true) {
-    console.log(d); // 13
-    let e = 23;
-    console.log(e); // 23
-}
-// console.log(e); // error
 
-var constant = 34;
-console.log(constant); // 34
-if(true) {
-    console.log(constant); // 34
-}
 
 
 
