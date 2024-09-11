ff3c9d96dff8f9d1e44a989a03ef9901472839ee
Ashish Singh <singhashish82103@gmail.com>
2024-09-11 12:58:10 +0530

    Initial commit

--- InClass/Lect5/script.js
+++ InClass/Lect5/script.js
@@ -2,7 +2,11 @@ var a = 12;
 var b = 13;
 
 function abc() {
-    var a = 10;
+    var a = 100;
+    console.log(a); // 100
+    console.log(b); // 13
 }
 
 abc();
+console.log(a); // 12
+console.log(b); // 13
