f2dbb96690566bd63b379eb0cbee4f9b4aa2e69f
Ashish Singh <singhashish82103@gmail.com>
2024-09-11 14:25:35 +0530

    Initial commit

--- InClass/Lect5/script.js
+++ InClass/Lect5/script.js
@@ -1,12 +1,24 @@
-var a = 12;
-var b = 13;
+// var a = 12;
+// var b = 13;
 
-function abc() {
-    var a = 100;
-    console.log(a); // 100
-    console.log(b); // 13
+// function abc() {
+//     var a = 100;
+//     console.log(a); // 100
+//     console.log(b); // 13
+// }
+
+// abc();
+// console.log(a); // 12
+// console.log(b); // 13
+
+// ============ try and catch ========================
+console.log('hello');
+
+function sum(a, b) {
+    //  Do something 
+    throw Error('Refernce Error due to non aligmnet'); 
+    return a + b;
+
 }
-
-abc();
-console.log(a); // 12
-console.log(b); // 13
+let result = sum(10, 20);
+console.log(result);
