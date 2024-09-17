e6351b77ada9b144741d7fefaf12644f971a338f
Ashish Singh <singhashish82103@gmail.com>
2024-09-17 08:27:35 +0530

    Initial commit

--- InClass/Lect5/script.js
+++ InClass/Lect5/script.js
@@ -29,40 +29,7 @@ console.log(a);
 let itemIcons = 34;
 // console.log();
 
-console.log("execution form");
-if(true) {
-    console.log("execution will be commited in this rule");
-} else {
-    console.log("execution will not be committed not will exeplenary");
-}
 
-let promiseOne = new Promise((resolve, reject)=> {
-    let items = null;
-    setInterval(() =>{
-        if( items === null) {
-            console.log("No Items found");
-            resolve("Error not getting find");
-        } else {
-            items = [1, 3]
-            console.log("Items Found");
-            reject(items, "Error", error); 
-        }
-    }, 2000)
-})
-promiseOne
-.then((result) => {
-    console.log(result, "result");
-
-})
-.catch((error) =>{
-    console.log("error", error);
-})
-.finally(() =>{
-    console.log("finllay getting executed")
-})
-
-const exampleOfTh  = "singhashish@82103gmail.com"; 
-const nextPath = 
 
 
 
