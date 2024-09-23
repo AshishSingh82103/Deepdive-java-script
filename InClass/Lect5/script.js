08698957ca723605a1df8f88857fdd8c1ebd8048
Ashish Singh <singhashish82103@gmail.com>
2024-09-23 14:58:03 +0530

    Initial commit

--- InClass/Lect5/script.js
+++ InClass/Lect5/script.js
@@ -12,7 +12,7 @@
 // console.log(b); // 13
 
 // ============ try and catch(debugg the code) ========================>
-console.log('hello');
+    console.log('hello');
 
 function sum(a, b) {
     //  Do something 
@@ -48,45 +48,31 @@ console.log(arr[1]); // 2
 
 // "git checkout -b BTS-6-sample-resolve-api-timeout-issues" 
 
-let promiseOne = new Promise((resolve, reject) => {
-    let items = "valuedetremination";
-    setInterval(() => {
-        if(items != String) {
-            console.log("is frameWorking");
-            resolve();
-        } else {
-            console.log("API TimeOut issue")
-            reject('error');
-        }
-    }, 2000)
+// let promiseOne = new Promise((resolve, reject) => {
+//     let items = "valuedetremination";
+//     setInterval(() => {
+//         if(items != String) {
+//             console.log("is frameWorking");
+//             resolve();
+//         } else {
+//             console.log("API TimeOut issue")
+//             reject('error');
+//         }
+//     }, 2000)
 
-})
-promiseOne
-.then((getFecth) => {
-    console.log("is frameWorking", getFecth);
-})
-.catch((error) => {
-    console.log("API TimeOut issue", error)
-})
-.finally(() => {
-    console.log("Execution is finally over");
-})
-
-
+// })
+// promiseOne
+// .then((getFecth) => {
+//     console.log("is frameWorking", getFecth);
+// })
+// .catch((error) => {
+//     console.log("API TimeOut issue", error)
+// })
+// .finally(() => {
+//     console.log("Execution is finally over");
+// })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
+let userLoggedIn = true;
+let userId = "singhashish832109tcs@gmail.com";
+let password = "Singh@82103";
