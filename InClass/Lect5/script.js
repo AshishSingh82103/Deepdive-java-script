ed604d0aa5823bb375554427960204ed30166647
Ashish Singh <singhashish82103@gmail.com>
2024-09-24 15:43:18 +0530

    Initial commit

--- InClass/Lect5/script.js
+++ InClass/Lect5/script.js
@@ -31,11 +31,31 @@ let items = 34;
 console.log(items);
 
 let userID = "singhashish82103@gmail.com";
-let userlogIn! = true;
+let userlogIn = true;
 let password = "singh@82103";
 
 if(userID == "singhashish82103@gmail.com") {
     console.log("conditional satisfactory ");   
-} else if() {
-    console.log()
+} else if(userlogIn != ture) {
+    console.log("conditon ternary operation is successsfully executed");
+} else if (password === "singh@82103") {
+    console.log("is back with the main string")
 }
+
+let idGenrate = "ashish832109tcs@gmail.com";
+for (let i = 0; i < idGenrate.stringLenght; i++){
+    console.log(i);
+}
+console.log(idGenrate.length); // 25
+
+let str = " String";
+console.log(str.length);
+
+let concatinate = idGenrate + str;
+console.log(concatinate);
+
+str.charAt(2);
+// console.log(str.charAt(2));  
+
+str.substring(0, 2); 
+console.log(str);
