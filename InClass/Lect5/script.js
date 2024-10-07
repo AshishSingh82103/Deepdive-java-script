1bd0b7b48cc732d563587b839383b0bc6e26b91c
Ashish Singh <singhashish82103@gmail.com>
2024-10-07 12:19:57 +0530

    Initial commit

--- InClass/Lect5/script.js
+++ InClass/Lect5/script.js
@@ -23,12 +23,13 @@ function sum(a, b) {
     try {
         // do something
         throw Error('Refernce Error due to non aligmnet'); 
-        let c = a + b;
-        return c;
+    let c = a + b;
+    return c;
 
     } catch(error) {
-        alert('Something there'); // error handeling gracefully
-    console.log("hey there user how are you doing") // output-printed
+        // alert('Something there'); // error handeling gracefully
+        console.log(error)
+        console.log("hey there user how are you doing") // output-printed
     // screen uhhh something went wrong
    
 }
@@ -39,3 +40,30 @@ function sum(a, b) {
 let result = sum(10, 20);
 console.log(result); // 30
 
+//  ==========function calcilator==============
+function calculator(a, b, action) {
+    try {
+        // throw Error('refernce error due to non-alignment')
+        let res;
+        if(action === 'add') {
+            // Google - server  google error add
+            res = a + b;
+        } else if(action === 'sub') {
+            // Microsoft - server  microsoft error add
+            res = a - b;
+        } else if(action === 'multiplie') {
+            // Digital - ocean server  digital ocean add
+            res = a * b;
+        }
+        return res;
+
+    } catch(error) {
+        console.log("error is here");
+    }
+}
+
+let result1 = calculator(1, 2, 'add');
+let result2 = calculator(1, 2, 'sub');
+let result3 = calculator(1, 2, 'multiplie');
+
+console.log(result1, result2, result3);
