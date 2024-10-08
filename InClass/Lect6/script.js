c11823ac771903cba1066decf1ce9a83a6c164b8
Ashish Singh <singhashish82103@gmail.com>
2024-10-08 10:54:41 +0530

    Initial commit

--- InClass/Lect6/script.js
+++ InClass/Lect6/script.js
@@ -1,30 +1,5 @@
 // ------------ hoisting ------------
 
-console.log(a);
+console.log(a); // undefined
 var a = 25;
 
-function calculator(a, b, action) {
-    try {
-        let res;
-    if(action === 'add') {
-        res = a + b;
-    } else if(action === 'sub') {
-        res = a - b;
-    } else if(action === 'multiplie') {
-        res = a * b;
-    } else if(action === 'division') {
-        res = a % b
-    }
-    return res;
-    }catch(error) {
-        
-    }
-}
-let result1 = calculator(5, 10, 'add');
-console.log(result1);
-let result2 = calculator(10, 5, 'add');
-console.log(result2);
-let result3 = calculator(5, 10, 'add');
-console.log(result3);
-let result4 = calculator(10, 5, 'add');
-console.log(result4);
