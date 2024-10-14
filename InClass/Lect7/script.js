68edc215c277a090aba05f9be52378ca42b77c52
Ashish Singh <singhashish82103@gmail.com>
2024-10-14 14:18:37 +0530

    initial commit

--- InClass/Lect7/script.js
+++ InClass/Lect7/script.js
@@ -12,11 +12,36 @@
 // var a = 12;
 // console.log(a);
 
-function sum(a, b) {
-    var c = a + b
-    return c;
-}
+// function sum(a, b) {
+//     var c = a + b
+//     return c;
+// }
 
-const result = sum(1, 2);
-console.log(result);
+// const result = sum(1, 2);
+// console.log(result);
 
+// --------------- Arrays ----------------
+
+let arr = [1, 2, 3, 4, 5];
+
+arr.push(6);
+
+console.log(arr);
+
+arr.pop();
+
+console.log(arr);
+
+
+// ================== First-class Function ================
+// A programming language is said to have First-class functions when functions in that language are treated like any other variable. For example, in such a language, a function can be passed as an argument to other functions, can be returned by another function and can be assigned as a value to a variable.
+
+// Examples
+// Assigning a function to a variable
+// js
+
+const foo = () => {
+  console.log("foobar");
+};
+foo(); // Invoke it using the variable
+// foobar
