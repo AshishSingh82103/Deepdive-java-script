05457e02252d5259db5440217e1c87383635cd87
Ashish Singh <singhashish82103@gmail.com>
2024-10-15 10:17:29 +0530

    Initial commit

---
+++ InClass/Lect8/script.js
@@ -0,0 +1,32 @@
+// function loggin() {
+//     // log
+//     console.log('loggin');
+
+// }
+
+// function sum(a, b) {
+//     return a + b;
+
+// }
+
+// loggin();
+// console.log(sum(2, 3));
+
+
+// requirement -> need to log first then you need to add.
+function loggin(callBackFunc) {
+    // log
+    console.log('loggin'); // loggin
+    callBackFunc(1, 2);
+
+}
+
+function sum(a, b) {
+    console.log(a + b); // 3
+    return a + b;
+
+}
+
+loggin(sum);
+
+
