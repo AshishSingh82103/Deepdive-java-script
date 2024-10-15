e23c558a100168ddb69276aa5ba0ab1af855d6a3
Ashish Singh <singhashish82103@gmail.com>
2024-10-15 11:25:35 +0530

    Initial commit

--- InClass/Lect8/script.js
+++ InClass/Lect8/script.js
@@ -14,17 +14,67 @@
 
 
 // requirement -> need to log first then you need to add.
-function loggin(callBackFunc) {
-    // log
-    console.log('loggin'); // loggin
-    callBackFunc(1, 2);
-}
+// function loggin(callBackFunc) {
+//     // log
+//     console.log('loggin'); // loggin
+//     callBackFunc(1, 2);
+// }
 
-function sum(a, b) {
-    console.log(a + b); // 3
+// function sum(a, b) {
+//     console.log(a + b); // 3
+//     return a + b;
+// }
+// loggin(sum); 
+
+
+
+// // calculator
+
+// function calculator(operation) {
+//     if(operation === 'add') {
+//         return function(a, b) {
+//             return a + b
+//         }
+
+//     }
+// }
+// calculator('add');
+
+
+// 1. function declartion ---------------->
+function add(a, b) {
     return a + b;
 }
-loggin(sum); 
+let rest = add(2,2);
+console.log(rest);
+
+// 2. function expression ---------------->
+
+const sum = function(a, b) {
+    return a + b;
+}
+ let result = sum(100, 2);
+ console.log(result);
+
+
+//  sum() diferentiate sum => browser result
+// const abc = function() {
+//     console.log('hey there');
+// }
+
+// abc
+// ƒ () {
+//     console.log('hey there');
+// }
+
+// abc();
+// VM172:2 hey there
+// undefined
+ 
+
+
+
+
 
 
 
