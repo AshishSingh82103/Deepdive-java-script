ccbd769fa4040c927e5ef4cb62bee29bb94e6dca
Ashish Singh <singhashish82103@gmail.com>
2024-10-16 14:31:53 +0530

    Initial commit

---
+++ InClass/Lect10/script.js
@@ -0,0 +1,54 @@
+//  --------------- Extra function Array callbacks ---------
+
+// 1. filter
+
+// let arrTwo = [5, 8, 900, 3, 1, 2, 18, 19, 20, 25, 100, 189];
+
+// function filterAshihs() {
+
+// }
+
+// // arrTwo.filter(function() {
+
+// // })'
+
+// // arrow - function
+// let ans = arrTwo.filter((item) => {
+//     return item > 19;
+// })
+
+// console.log(ans); // [ 900, 20, 25, 100, 189 ]
+
+
+
+// 1. for each
+
+let arr = [19, 21, 37, 44, 51, 69, 77, 89];
+
+// 1. parent => forEach
+// => for each going to evry index
+
+// 2. callback -> () =>{printout the element} // user function
+
+
+arr.forEach((value, index, arr) => {
+    console.log(value, index, arr, 'hey there i am traversing');
+
+})
+
+// function sum() {
+
+// }
+// sum() // me calling
+
+
+
+
+
+
+
+
+
+
+
+
