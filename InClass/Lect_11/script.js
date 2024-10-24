528a58ef3249e7482c0085f33c2cb01e67a0a515
Ashish Singh <singhashish82103@gmail.com>
2024-10-24 13:54:02 +0530

    Initial commit

--- InClass/Lect_11/script.js
+++ InClass/Lect_11/script.js
@@ -1,10 +1,37 @@
-console.time("answer time");
+// console.time("answer time");
 
-alert("Click to continue");
+// alert("Click to continue");
 
-console.timeLog("answer time");
+// console.timeLog("answer time");
 
-alert("Do a bunch of other stuff…");
+// alert("Do a bunch of other stuff…");
 
-console.timeEnd("answer time");
+// console.timeEnd("answer time");
+
+
+// ---------------- Dom (Data object manipulation) --------------
+
+let documents = document.querySelector('h1');
+
+let inputButtons = document.querySelector('.styled');
+
+let outputButtons = documents.querySelector('.styled_1');
+
+documents.style.backgroundColor = "yellow";
+documents.style.padding = "12px";
+documents.style.margin = "20px";
+
+inputButtons.style.backgroundColor = "red";
+inputButtons.style.border = "2px";
+
+
+
+
+
+
+    
+
+
+
+
 
