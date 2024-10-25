c3a01f605ac3b0aa861c78b0c30e5f9faf7c0f46
Ashish Singh <singhashish82103@gmail.com>
2024-10-25 10:20:14 +0530

    initial commit

--- InClass/Lect_11/script.js
+++ InClass/Lect_11/script.js
@@ -21,6 +21,13 @@ documents.style.backgroundColor = "yellow";
 documents.style.padding = "12px";
 documents.style.margin = "20px";
 
+inputButtons.style.backgroundColor = "blue";
+inputButtons.style.padding = "25px";
+inputButtons.style.margin = '12px';
+inputButtons.style.borderRadius = '10px';
+inputButtons.style.border = 'solid 2px red';
+
+
 
 
 
