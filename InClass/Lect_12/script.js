dfc0554619d37b670d20b765f372223f6d7ba98d
Ashish Singh <singhashish82103@gmail.com>
2024-10-28 15:17:41 +0530

    Initial commit

--- InClass/Lect_12/script.js
+++ InClass/Lect_12/script.js
@@ -1,3 +1,12 @@
 function start() {
-var document_list = document.getElementById("")
+var document_list = document.getElementById("new_value");
+document_list.style.backgroundColor = "yellow";
 }
+
+//  manupulating the element
+
+myBody = document.getElementsByTagName('body [0]');
+
+mybody.appendChild(start);
+
+mybody.appendChild(end);
