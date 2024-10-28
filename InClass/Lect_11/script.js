417ac327a03ae49dc66a83b7b037d574fd4dcb69
Ashish Singh <singhashish82103@gmail.com>
2024-10-28 10:12:14 +0530

    Initial commit

--- InClass/Lect_11/script.js
+++ InClass/Lect_11/script.js
@@ -105,40 +105,52 @@ function genrateTable() {
 // ----------- function calling the genrate table ------------
 
 function genrateTable() {
-    //  create a tbale <element> and <tbody> a element.
-    const tb1 = document.createElement('table');
-    
-    const tb1Body = document.getElementById('tbody');
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
 
-    //  creating all cellss
+  // creating all cells
+  for (let i = 0; i < 2; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
 
-    for (let i = 0; i < 2; i++) {
-        // creates a table row
-        const row = document.createElement("tr");
-    
-        for (let j = 0; j < 2; j++) {
-          // Create a <td> element and a text node, make the text
-          // node the contents of the <td>, and put the <td> at
-          // the end of the table row
-          const cell = document.createElement("td");
-          const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-        }
-    
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
-      }
-    
-      // put the <tbody> in the <table>
-      tbl.appendChild(tblBody);
-      // appends <table> into <body>
-      document.body.appendChild(tbl);
-      // sets the border attribute of tbl to '2'
-      tbl.setAttribute("border", "2");
+    for (let j = 0; j < 2; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
     }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  document.body.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+}
+
+  //  ------------ set-backgound ------------
+  function setBackground() {
+    // Now get all the p element in the documents.
+    const paragraph = document.getElementsByTagName("p");
+
+    const secondParagraph = paragraph[1];
+
+    //  set the inline style
     
+    secondParagraph.style.backgroundColor = 'red';
+
     
+  }
+    
+
 
 
 
