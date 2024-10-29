73519e540bf1bcbb64b4746282d7acf2642b11ac
Ashish Singh <singhashish82103@gmail.com>
2024-10-29 13:48:13 +0530

    Initial commit

--- InClass/Lect_13/script.js
+++ InClass/Lect_13/script.js
@@ -51,3 +51,17 @@ btn1.addEventListener('click', console.log('logoIn into the main value'));
 // console.log(Math.floor(Math.random() * (10 + 1)));
 
 console.log("new balnace");
+
+
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function bgChange() {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+}
+
+btn.onclick = bgChange;
