4dc7abe880ca31fec0cb9c10fe231fdf31b14ff4
Ashish Singh <singhashish82103@gmail.com>
2024-12-11 10:51:13 +0530

    Initial commit

--- InClass/Lect_11/script.js
+++ InClass/Lect_11/script.js
@@ -2,6 +2,12 @@
 
 // -------------- Dom => data object manipulation ------------
 
-let object = new Object({
-    
-})
+const para = document.getElementById("paragraph");
+para.style.backgroundColor = "yellow";
+
+const heading = document.getElementById('heading');
+heading.style.backgroundColor = "green";
+
+const body = document.getElementsByTagName('body');
+body.style.backgroundColor = "pink";
+
