9a210993f58d986ffa51e42b83a57105b534c35c
Ashish Singh <singhashish82103@gmail.com>
2024-12-13 12:37:44 +0530

    Initial commit

--- InClass/Lect_11/script.js
+++ InClass/Lect_11/script.js
@@ -2,19 +2,15 @@
 
 // -------------- Dom => data object manipulation ------------
 
-const para = document.getElementById("paragraph");
-para.style.backgroundColor = "yellow";
-para.style.padding = '15px';
-
-const heading = document.getElementById('heading');
-heading.style.backgroundColor = "green";
-heading.style.padding = "10px";
-
-const body = document.getElementsByTagName('body')[0];
-body.style.backgroundColor = "pink";
-
-const body = document.getElementsByTagName('body')[0];
-body.document.style.backgOroundColor = "Iceblue";
+document.addEventListener('DOMContentLoaded', () => { 
+    const socialLinks = document.querySelectorAll('.social-media a'); 
+    socialLinks.forEach(link => { 
+        link.addEventListener('click', (e) => {
+             e.preventDefault(); 
+             alert(`Redirecting to ${link.textContent}...`); 
+            }); 
+        });
+    });
 
 
 
