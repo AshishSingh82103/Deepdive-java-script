c83b85ed2e8241d52892619e12b8cef85594d528
Ashish Singh <singhashish82103@gmail.com>
2024-12-18 11:02:51 +0530

    Initial commit

--- InClass/Lect_12/script.js
+++ InClass/Lect_12/script.js
@@ -2,3 +2,27 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault(); 
     alert('Your message has been sent!'); 
 });
+
+// Smooth Scrolling for navigation Links
+document.querySelectorAll('nav a').foreach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoview({
+
+        });
+    });
+});
+
+// Dynamic Date in foooter
+const year = new Date().getFullYear();
+document.querySelector('footer p').textContent = `©${year} My Website. All rights reserved`;
+
+// Interactive Services Section
+const services = document.querySelectorAll('.service');
+services.forEach(service=>{
+    service.style.transform = 'scale(1.05)';
+    service.style.transition = 'transform 0.3s';
+});
+service.addEventListener('mouseleave', () => {
+    service.style.transform = 'scale(1)';
+})
